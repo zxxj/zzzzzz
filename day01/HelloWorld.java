cc786a83421e8c18bcdc77db0e9747998b38a9ef
zhangxinxin <1285367184@qq.com>
2024-07-11 00:00:34 +0800

    1.HelloWorld

---
+++ day01/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+	public: 代表公共的,公开的
+	class: 代表类
+	HelloWorld: 代表类的名字
+	public static void main(String[] args): 代表主方法,固定写法,会被jvm执行\
+	System.out.println: 输出语句
+*/
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+}
