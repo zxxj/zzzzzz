620bec6b3c7a520052f73905ddf77f1ab59e7432
zhangxinxin <1285367184@qq.com>
2024-07-11 23:16:30 +0800

    三种打印方式: println println的区别

---
+++ day01/PrintTest.java
@@ -0,0 +1,11 @@
+public class PrintTest {
+	public static void main(String[] args) {
+		
+		System.out.println("ÄãºÃ");
+		System.out.println();
+		System.out.println("ÊÀ½ç");
+		
+		System.out.print("Hello");
+		System.out.print("World");
+	}
+}
