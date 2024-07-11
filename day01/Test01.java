f207457780cbb1946118645431f26144dddb5e32
zhangxinxin <1285367184@qq.com>
2024-07-11 23:15:54 +0800

    第一个程序练习: 打印姓名籍贯和爱好

---
+++ day01/Test01.java
@@ -0,0 +1,23 @@
+/**
+	第一个程序的练习: 
+	创建三个类
+	打印你的名字,籍贯和爱好
+*/
+
+public class Test01 {
+	public static void main(String[] args) {
+		System.out.println("姓名: zhangxinxin");
+	}
+}
+
+class Address {
+	public static void main(String[] args) {
+		System.out.println("性别: 男");
+	}
+}
+
+class Hobby {
+	public static void main(String[] args) {
+		System.out.println("爱好: 打羽毛球");
+	}
+}
